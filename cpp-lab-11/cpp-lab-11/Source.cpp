2fc1455a7f8a2f837ae3ef7a63c120d41023d58d
alinapochinova <alinapochinova@mail.ru>
2024-02-20 21:26:47 +0300

    Update Source.cpp

--- cpp-lab-11/cpp-lab-11/Source.cpp
+++ cpp-lab-11/cpp-lab-11/Source.cpp
@@ -24,14 +24,16 @@ void ContainerAlgorithms() {
     advance(it, -3);//продвинули итератор
     it = myList.erase(it); // удалили элемент
 
+    add.remove(3);
+
     advance(it, 5);
     myList.splice(it, add); // переносим элементы из одного списка в другой, вставл€ем элементы туда, где находитс€ итератор(второй список становитьс€ пустым)
-
-    cout << "—писок: [ ";
+    //splice только у списков 
+    cout << "—писок: ";
     for (list<int>::const_iterator i = myList.cbegin(); i != myList.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout  << endl;
 }
 
 void AdapterIterators() {
@@ -40,20 +42,20 @@ void AdapterIterators() {
     for (int i = 1; i <= 10; i++) {
         myVector.push_back(i);
     }
-    cout << "Ёлементы изначального вектора: [";
+    cout << "Ёлементы изначального вектора: ";
     for (vector<int>::const_iterator i = myVector.cbegin(); i != myVector.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout << endl;
 
     // fill_n() присваивает count элементам из диапазона [first,first+count) значение value.
 
     fill_n(back_inserter(myVector), 3, 100);// добавл€ем в конец вектора три 100
-    cout << "Ёлементы вектора после добавлени€ трЄх '100' в конец: [";
+    cout << "Ёлементы вектора после добавлени€ трЄх '100' в конец: ";
     for (vector<int>::const_iterator i = myVector.cbegin(); i != myVector.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout  << endl;
      
     /*fill_n(front_inserter(myVector), 5, 333);// добавл€ем в начало вектора п€ть 333
     cout << "Ёлементы списка после добавлени€ п€ти '333' в начало: [";
@@ -61,50 +63,52 @@ void AdapterIterators() {
         cout << *i << " ";
     }
     cout << "]" << endl;*/
+
+
     vector<int>::iterator forvector = myVector.begin();
 
     advance(forvector, 10);
 
     fill_n(inserter(myVector, forvector), 2, 999);
 
-    cout << "Ёлементы вектора после добавлени€ двух '999' перед дес€тым элементом: [";
+    cout << "Ёлементы вектора после добавлени€ двух '999' перед дес€тым элементом: ";
     for (vector<int>::const_iterator i = myVector.cbegin(); i != myVector.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout << endl;
 
     copy(myVector.begin(), myVector.end(), front_inserter(myList));
-    cout << "Ёлементы списка после копировани€ из вектора с конца: [";
+    cout << "Ёлементы списка после копировани€ из вектора с конца: ";
     for (list<int>::const_iterator i = myList.cbegin(); i != myList.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout << endl;
 
     
     fill_n(front_inserter(myList), 5, 333);// добавл€ем в начало списка п€ть 333
-    cout << "Ёлементы списка после добавлени€ п€ти '333' в начало: [";
+    cout << "Ёлементы списка после добавлени€ п€ти '333' в начало: ";
     for (list<int>::const_iterator i = myList.cbegin(); i != myList.cend(); ++i) {
         cout << *i << " ";
     }
-    cout << "]" << endl;
+    cout << endl;
 }
 
-void Files() {
+void File() {
     // чтение из файла
     vector <string> myVector;
     ifstream file("/Users/јлина/Desktop/TextFile1.txt");
     copy(istream_iterator<string>(file), istream_iterator<string>(), back_inserter(myVector));
     file.close();
-    cout << "ѕрочтенный из файла вектор: [";
+    cout << "ѕрочтенный из файла вектор: ";
     // copy создает потоковый итератор вывода, который записывает string в выходной поток данных cout, вызыва€ оператор << дл€ каждого элемента
     copy(myVector.begin(), myVector.end(), ostream_iterator<string>(cout, " "));
-    cout << "]" << endl;
+    cout << endl;
 
     //запись в файл
     vector <string> myVector2 = { "67", "345", "12", "-45", "0", "-7" };
     ofstream file2;
     file2.open("/Users/јлина/Desktop/TextFile2.txt");
-    copy(myVector2.begin(), myVector2.end(), ostream_iterator<string>(file2, " "));
+    copy(myVector2.begin(), myVector2.end(), ostream_iterator<string>(file2, " ")); // " " значит что между выводимыми элементами стоит пробел
     file2.close();
     cout << endl;
 }
@@ -113,12 +117,12 @@ void IteratorCompatibility() {
     vector <int> myVector = { 8, 8, 0, 0, 5, 5, 5, 3, 5, 3, 5 };
     deque<int> myDeck;
     copy(myVector.begin(), myVector.end(), back_inserter(myDeck));
-    cout << "Ёлементы вектора: [ ";
-    copy(myVector.begin(), myVector.end(), ostream_iterator<int>(cout, " "));
-    cout << "]" << endl;
-    cout << "Ёлементы дека: [ ";
+    cout << "Ёлементы вектора: ";
+    copy(myVector.begin(), myVector.end(), ostream_iterator<int>(cout, " ")); 
+    cout << endl;
+    cout << "Ёлементы дека: ";
     copy(myDeck.begin(), myDeck.end(), ostream_iterator<int>(cout, " "));
-    cout << "]" << endl;
+    cout <<  endl;
 }
 
 void ReverseIterator() {
@@ -129,12 +133,12 @@ void ReverseIterator() {
         cin >> temp;
         myList.push_back(temp);
     }
-    cout << "—писок в обратном пор€дке: [ ";
+    cout << "—писок в обратном пор€дке: ";
     for (list<int>::const_reverse_iterator i = myList.crbegin(); i != myList.crend(); ++i) {
         cout << *i << " ";
     }
    
-    cout << "]" << endl;
+    cout  << endl;
 }
 
 int main() {
@@ -144,7 +148,7 @@ int main() {
     cout << "»тераторы-адаптеры" << endl;
     AdapterIterators();
     cout << "‘айловый ввод-вывод с итераторами" << endl;
-    Files();
+    File();
     cout << "—овместимость итераторов" << endl;
     IteratorCompatibility();
     cout << "ќбратные итераторы" << endl;
